19ed24d22a4fa8bad8b53eb047206a4fe61a5486
Romain ROCHAS <romainrochas69@gmail.com>
2026-09-09 16:10:53 +0200

    update(analytics): ajouter balise Umami
    Signed-off-by: Romain ROCHAS <romainrochas69@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,11 @@ export default function RootLayout({
           <meta name="apple-mobile-web-app-title" content="Scouticket" />
           <meta name="mobile-web-app-capable" content="yes" />
           <link rel="manifest" href="/manifest.json" />
+          <script
+            defer
+            src="https://analytics.scouticket.fr/script.js"
+            data-website-id="4260f7db-3623-438a-95a4-9ba2631bcc79"
+          />
         </head>
         <body className="font-sans">
           <div className="min-h-screen">{children}</div>
